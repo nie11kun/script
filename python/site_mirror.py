1ed465383e3ecf028d08955c657b3badd81d1466
Marco Nie <github@niekun.net>
2019-11-15 20:13:01 +0800

    update

--- python/site_mirror.py
+++ python/site_mirror.py
@@ -10,33 +10,38 @@ logger = logging.getLogger(__name__)
 site = 'niekun.net'
 
 windows_path = 'C:\\Users\\HJ_NK\\Documents\\'
+windows_slash = '\\'
 
 macOS_path = '/Users/marconie/Public/GitHub/nie11kun.github.io/'
+macOS_slash = '/'
 
 if platform.system() == 'Windows':
     path = windows_path
-    fileInFolder = '\\.'
+    slash = windows_slash
 elif platform.system() == 'Darwin':
     path = macOS_path
-    fileInFolder = '/.'
+    slash = macOS_slash
 else:
     path = macOS_path
-    fileInFolder = '/.'
+    slash = macOS_slash
 
 tempPath = dirname(path)
 
 logger.info('start mirror site: {}, output directory is {}'.format(site, path))
+
+logger.info('sync remote github io')
+os.system('cd {} && git checkout . && git pull'.format(path))
 
 try:
     os.system('cd {} && wget -m -p -k {}'.format(tempPath, site))
 except:
     logger.info('error')
 else:
-    tempPath = tempPath + site
-    os.system('cp -r {} {}'.format(tempPath + fileInFolder, path))
-    os.system('rm -r {}'.format(tempPath))
-
     logger.info('download finished, pushing to github io')
+
+    tempPath = tempPath + slash + site
+    os.system('cp -r {} {}'.format(tempPath + slash + '.', path))
+    os.system('rm -r {}'.format(tempPath))
 
     os.system('cd {} && git add . && git commit -m "{}" && git push'.format(path, datetime.datetime.now()))
 
