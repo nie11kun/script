fecaca8d87f3981fe858a59cd39a6fc8b542eff4
Marco Nie <github@niekun.net>
2019-11-19 13:44:14 +0800

    fix

--- python/site_mirror.py
+++ python/site_mirror.py
@@ -60,30 +60,24 @@ else:
     p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE).communicate()
     it = iter(str(p[0], 'utf-8').split('\n'))
 
+    logger.info('moving new files')
+    print('moving new files')
+    tempPath = tempPath + slash + site
+    os.system('cp -r {} {}'.format(tempPath + slash + '.', path))
+    os.system('rm -r {}'.format(tempPath))
+
+    logger.info('pushing to github io')
+    print('pushing to github io')
+
+    cmd = 'cd {} && git add . && git commit -m "{}" && git push'.format(path, datetime.datetime.now())
+    p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE).communicate()
+    it = iter(str(p[0], 'utf-8').split('\n'))
+
     try:
-        while "Total wall clock time" not in next(it): pass
+        while "Everything up-to-date" not in next(it): pass
     except StopIteration:
-        logger.error('mirror error')
-        print('mirror error')
+        logger.error('push error')
+        print('push error')
     else:
-        logger.info('moving new files')
-        print('moving new files')
-        tempPath = tempPath + slash + site
-        os.system('cp -r {} {}'.format(tempPath + slash + '.', path))
-        os.system('rm -r {}'.format(tempPath))
-
-        logger.info('pushing to github io')
-        print('pushing to github io')
-
-        cmd = 'cd {} && git add . && git commit -m "{}" && git push'.format(path, datetime.datetime.now())
-        p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE).communicate()
-        it = iter(str(p[0], 'utf-8').split('\n'))
-
-        try:
-            while "Everything up-to-date" not in next(it): pass
-        except StopIteration:
-            logger.error('push error')
-            print('push error')
-        else:
-            logger.info('push finished')
-            print('push finished')
+        logger.info('push finished')
+        print('push finished')
