fd6d4dca07ed036da8d9e74f3adc07766107432d
Marco Nie <github@niekun.net>
2019-11-20 21:12:05 +0800

    test

--- python/site_mirror.py
+++ python/site_mirror.py
@@ -63,10 +63,10 @@ else:
 
     cmd = 'cd {} && wget -m -p -k {}'.format(tempPath, site)
     p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE).communicate()
-    p[0] = p[0].decode('cp936').encode('utf-8')
-    it = iter(p[0].split('\n'))
+    p_str = p[0].decode('cp936').encode('utf-8')
+    it = iter(p_str.split('\n'))
     print(p[0])
-    
+
     logger.info('moving new files')
     print('moving new files')
     tempPath = tempPath + slash + site
