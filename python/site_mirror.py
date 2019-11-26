1e6e48ebb3e99d2a3b2e2ba5e8e7db72b0c93b7e
Marco Nie <github@niekun.net>
2019-11-26 12:00:40 +0800

    update

--- python/site_mirror.py
+++ python/site_mirror.py
@@ -1,14 +1,9 @@
-# -*- coding: utf-8 -*- 
-#import sys
 import platform
 import os
 import logging
 import datetime
 from os.path import dirname
 from subprocess import Popen, PIPE
-
-#reload(sys)
-#sys.setdefaultencoding('utf-8')
 
 site = 'niekun.net'
 
@@ -63,20 +58,32 @@ else:
 
     cmd = 'cd {} && wget -m -p -k {}'.format(tempPath, site)
     p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE).communicate()
-    it = iter(str(p[0], 'utf-8').split('\n'))
+    it = iter(str(p[1], 'utf-8').split('\n'))
 
-    logger.info('moving new files')
-    print('moving new files')
-    tempPath = tempPath + slash + site
-    os.system('cp -r {} {}'.format(tempPath + slash + '.', path))
-    os.system('rm -r {}'.format(tempPath))
+    try:
+        while "FINISHED" not in next(it): pass
+    except StopIteration:
+        logger.error('download error')
+        print('download error')
+    else:
+        logger.info('moving new files')
+        print('moving new files')
+        tempPath = tempPath + slash + site
+        os.system('cp -r {} {}'.format(tempPath + slash + '.', path))
+        os.system('rm -r {}'.format(tempPath))
 
-    logger.info('pushing to github io')
-    print('pushing to github io')
+        logger.info('pushing to github io')
+        print('pushing to github io')
 
-    cmd = 'cd {} && git add . && git commit -m "{}" && git push'.format(path, datetime.datetime.now())
-    p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE).communicate()
-    it = iter(str(p[0], 'utf-8').split('\n'))
+        cmd = 'cd {} && git add . && git commit -m "{}" && git push && git status'.format(path, datetime.datetime.now())
+        p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE).communicate()
+        it = iter(str(p[1], 'utf-8').split('\n'))
 
-    logger.info('push finished')
-    print('push finished')
+        try:
+            while "Your branch is up-to-date" not in next(it): pass
+        except StopIteration:
+            logger.error('push error')
+            print('push error')
+        else:
+            logger.info('push finished')
+            print('push finished')
