0014206ea2fb375ab15bedcd548e8d8ee1a69b8d
Marco Nie <github@niekun.net>
2020-02-19 10:09:00 +0800

    fix

--- bash/autoremove_log.sh
+++ bash/autoremove_log.sh
@@ -4,6 +4,6 @@ LogDir[1]="/var/log/v2ray/"
 LogFile[0]="access.log"
 LogFile[1]="error.log"
 
-rm ${LogDir[*]}${LogFile[*]}*
+rm ${LogDir[*]}${LogFile[*]}* && echo delete done
 
-cat /dev/null ${LogDir[*]}${LogFile[*]}
+cat /dev/null ${LogDir[*]}${LogFile[*]} && echo clean done
