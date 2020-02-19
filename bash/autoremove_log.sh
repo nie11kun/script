bc92e7bc6d13eba39b19f51f731a6c5ac4d25f1e
Marco Nie <github@niekun.net>
2020-02-19 10:16:30 +0800

    fix

--- bash/autoremove_log.sh
+++ bash/autoremove_log.sh
@@ -3,6 +3,6 @@ LogFile[1]="/var/log/nginx/error.log"
 LogFile[2]="/var/log/v2ray/access.log"
 LogFile[3]="/var/log/v2ray/error.log"
 
-rm ${LogFile[*]}*
+rm ${LogFile[*]}.*
 
 cat /dev/null ${LogFile[*]}
