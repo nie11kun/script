d81018191c35f3d9003f4c44828655ca4ff99204
Marco Nie <github@niekun.net>
2020-10-14 09:45:46 +0800

    update

--- windows/v2ray_autorun.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-if "%1" == "h" goto begin
-mshta vbscript:createobject("wscript.shell").run("""%~nx0"" h",0)(window.close)&&exit
-:begin
-REM
-cd "C:\Users\Marco Nie\Application\v2ray"
-v2ray -confdir conf.d
-exit
---
+++ windows/wsl_ip.ps1
@@ -0,0 +1,9 @@
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))  
+{  
+  $arguments = "& '" +$myinvocation.mycommand.definition + "'"
+  Start-Process powershell -Verb runAs -ArgumentList $arguments
+  Break
+}
+
+wsl -d Ubuntu -u root ip addr add 192.168.50.16/24 broadcast 192.168.50.255 dev eth0 label eth0:1
+netsh interface ip add address "vEthernet (WSL)" 192.168.50.88 255.255.255.0
