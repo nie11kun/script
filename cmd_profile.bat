d751079199bcc702962a2571a28b6567bc33ef4d
Marco Nie <github@niekun.net>
2021-01-06 10:50:58 +0800

    update

---
+++ cmd_profile.bat
@@ -0,0 +1,5 @@
+@echo off
+doskey phttp = set http_proxy=http://127.0.0.1:1082
+doskey phttps = set https_proxy=http://127.0.0.1:1082
+doskey unphttp = set http_proxy=
+doskey unphttps = set https_proxy=
