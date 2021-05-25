f5e7ac1f32d4e5fa68e0d639cbae564d4aa9db32
Marco Nie <github@niekun.net>
2021-05-25 08:11:26 +0800

    update

--- bash/submit_CommitCodeBot.sh
+++ bash/submit_CommitCodeBot.sh
@@ -2,6 +2,6 @@
 
 LOGFILE="/home/log/submit_code.log"
 
-telegram-cli -W -e "msg @LvanLamCommitCodeBot /jdcrazyjoy wIgzEM1_9ferfc3eWGgXhA==" >> ${LOGFILE}
-telegram-cli -W -e "msg @LvanLamCommitCodeBot /jdzz S4KQmQR9CsAo" >> ${LOGFILE}
-telegram-cli -W -e "msg @LvanLamCommitCodeBot /jdcash fUJibe3pI6I" >> ${LOGFILE}
+telegram-cli -W -e "msg @LvanLamCommitCodeBot /jdcrazyjoy wIgzEM1_9ferfc3eWGgXhA=="
+telegram-cli -W -e "msg @LvanLamCommitCodeBot /jdzz S4KQmQR9CsAo"
+telegram-cli -W -e "msg @LvanLamCommitCodeBot /jdcash fUJibe3pI6I"
---
+++ bash/submit_TuringLabbot_daily.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+LOGFILE="/home/log/submit_code.log"
+
+telegram-cli -W -e "msg @TuringLabbot /submit_activity_codes city Qtywye3_BFTvMs-aW5h_j9PsJByXxAFyFvu6gmA"
+telegram-cli -W -e "msg @TuringLabbot /submit_activity_codes carnivalcity 1cdc8fb4-4eab-4ab5-be11-8c51945ea2c7"
