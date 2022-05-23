d01d4c82d55c859205c08f1a7ca9a2b702bbda69
Marco Nie <github@niekun.net>
2022-05-23 17:26:14 +0800

    add my personal cfspeedtest url

--- windows/cfip.bat
+++ windows/cfip.bat
@@ -16,8 +16,8 @@ git pull
 echo.
 echo test ip list:
 cd "C:\Users\Marco Nie\Application\CloudflareST_windows_amd64"
-".\CloudflareST.exe" -p 0 -f "C:\Users\Marco Nie\Development\VPN\cloudflare\Google Cloud.txt" -o "result-google.csv"
-".\CloudflareST.exe" -p 0 -f "C:\Users\Marco Nie\Development\VPN\cloudflare\Amazon Web Services - 中国 香港.txt" -o "result-amazon.csv"
+".\CloudflareST.exe" -url https://niekun.net/cloud/other/cfspeedtest.png -p 0 -f "C:\Users\Marco Nie\Development\VPN\cloudflare\Google Cloud.txt" -o "result-google.csv"
+".\CloudflareST.exe" -url https://niekun.net/cloud/other/cfspeedtest.png -p 0 -f "C:\Users\Marco Nie\Development\VPN\cloudflare\Amazon Web Services - 中国 香港.txt" -o "result-amazon.csv"
 
 :: start notepad "result-google.csv"
 :: start notepad "result-amazon.csv"
