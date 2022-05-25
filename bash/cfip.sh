eb31911f00f9d0ed59763e814a6ab6ba93d47861
Marco Nie <github@niekun.net>
2022-05-25 19:49:44 +0800

    update

--- bash/cfip.sh
+++ bash/cfip.sh
@@ -7,8 +7,8 @@ git reset --soft HEAD^ && git reset && git checkout . && git add * && git stash 
 echo
 echo test ip list:
 cd "/Users/marconie/Apps/CloudflareST_darwin_amd64"
-./CloudflareST -p 0 -f "/Users/marconie/Development/VPN/cloudflare/Google Cloud.txt" -o "result-google.csv"
-./CloudflareST -p 0 -f "/Users/marconie/Development/VPN/cloudflare/Amazon Web Services - 中国 香港.txt" -o "result-amazon.csv"
+./CloudflareST -url https://niekun.net/cloud/other/cfspeedtest.png -p 0 -f "/Users/marconie/Development/VPN/cloudflare/Google Cloud.txt" -o "result-google.csv"
+./CloudflareST -url https://niekun.net/cloud/other/cfspeedtest.png -p 0 -f "/Users/marconie/Development/VPN/cloudflare/Amazon Web Services - 中国 香港.txt" -o "result-amazon.csv"
 
 echo
 echo finded ip:
