d1b4c1ac3fdd1406da790de6781c470bb03011c1
Marco Nie <github@niekun.net>
2022-06-15 22:06:46 +0800

    update

--- bash/cfip.sh
+++ bash/cfip.sh
@@ -10,12 +10,14 @@ cd "/Users/marconie/Apps/CloudflareST_darwin_amd64"
 ./CloudflareST -url https://niekun.net/cloud/other/cfspeedtest.png -p 0 -f "/Users/marconie/Development/VPN/cloudflare/Google Cloud.txt" -o "result-google.csv"
 ./CloudflareST -url https://niekun.net/cloud/other/cfspeedtest.png -p 0 -f "/Users/marconie/Development/VPN/cloudflare/Amazon Web Services - 中国 香港.txt" -o "result-amazon.csv"
 ./CloudflareST -url https://niekun.net/cloud/other/cfspeedtest.png -p 0 -f "/Users/marconie/Development/VPN/cloudflare/BGP Network - 中国 香港.txt" -o "result-bgp.csv"
+./CloudflareST -url https://niekun.net/cloud/other/cfspeedtest.png -p 0 -f "/Users/marconie/Development/VPN/cloudflare/POWER LINE - 中国 香港.txt" -o "result-powerline.csv"
 
 echo
 echo finded ip:
 cat "result-google.csv"
 cat "result-amazon.csv"
 cat "result-bgp.csv"
+cat "result-powerline.csv"
 
 echo
 echo ip list can be use:
@@ -34,6 +36,13 @@ awk -F, '/\./ {if($6 != "0.00") {print $1}}' result-amazon.csv | while read outp
 done
 
 awk -F, '/\./ {if($6 != "0.00") {print $1}}' result-bgp.csv | while read output; do
+    ping -c 1 "$output" >/dev/null
+    if [ $? -eq 0 ]; then
+        echo "$output is ok"
+    fi
+done
+
+awk -F, '/\./ {if($6 != "0.00") {print $1}}' result-powerline.csv | while read output; do
     ping -c 1 "$output" >/dev/null
     if [ $? -eq 0 ]; then
         echo "$output is ok"
