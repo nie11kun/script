1ce06e8dbfde967051451d7f6c9b89ea9a69acc8
Marco Nie <github@niekun.net>
2022-07-26 13:10:30 +0800

    update

--- windows/cfip.bat
+++ windows/cfip.bat
@@ -16,24 +16,39 @@ git pull
 echo.
 echo test ip list:
 cd "C:\Users\Marco Nie\Application\CloudflareST_windows_amd64"
-".\CloudflareST.exe" -url https://niekun.net/cloud/other/cfspeedtest.png -p 0 -f "C:\Users\Marco Nie\Development\VPN\cloudflare\Google Cloud.txt" -o "result-google.csv"
-".\CloudflareST.exe" -url https://niekun.net/cloud/other/cfspeedtest.png -p 0 -f "C:\Users\Marco Nie\Development\VPN\cloudflare\Amazon Web Services - 中国 香港.txt" -o "result-amazon.csv"
-".\CloudflareST.exe" -url https://niekun.net/cloud/other/cfspeedtest.png -p 0 -f "C:\Users\Marco Nie\Development\VPN\cloudflare\BGP Network - 中国 香港.txt" -o "result-bgp.csv"
-".\CloudflareST.exe" -url https://niekun.net/cloud/other/cfspeedtest.png -p 0 -f "C:\Users\Marco Nie\Development\VPN\cloudflare\POWER LINE - 中国 香港.txt" -o "result-powerline.csv"
+".\CloudflareST.exe" -url https://niekun.net/cloud/other/cfspeedtest.png -p 0 -f "C:\Users\Marco Nie\Development\VPN\cloudflare\Google Cloud - 日本 东京.txt" -o "result-google-jp.csv"
+".\CloudflareST.exe" -url https://niekun.net/cloud/other/cfspeedtest.png -p 0 -f "C:\Users\Marco Nie\Development\VPN\cloudflare\Google Cloud - 美国 阿什本.txt" -o "result-google-us.csv"
+".\CloudflareST.exe" -url https://niekun.net/cloud/other/cfspeedtest.png -p 0 -f "C:\Users\Marco Nie\Development\VPN\cloudflare\Amazon Web Services - 中国 香港.txt" -o "result-amazon-hk.csv"
+".\CloudflareST.exe" -url https://niekun.net/cloud/other/cfspeedtest.png -p 0 -f "C:\Users\Marco Nie\Development\VPN\cloudflare\Amazon Web Services - 日本 东京.txt" -o "result-amazon-jp.csv"
+".\CloudflareST.exe" -url https://niekun.net/cloud/other/cfspeedtest.png -p 0 -f "C:\Users\Marco Nie\Development\VPN\cloudflare\CTM - 中国 澳门.txt" -o "result-ctm.csv"
+".\CloudflareST.exe" -url https://niekun.net/cloud/other/cfspeedtest.png -p 0 -f "C:\Users\Marco Nie\Development\VPN\cloudflare\HGC - 中国 香港.txt" -o "result-hgc.csv"
+".\CloudflareST.exe" -url https://niekun.net/cloud/other/cfspeedtest.png -p 0 -f "C:\Users\Marco Nie\Development\VPN\cloudflare\HKT - 中国 香港.txt" -o "result-hkt.csv"
+".\CloudflareST.exe" -url https://niekun.net/cloud/other/cfspeedtest.png -p 0 -f "C:\Users\Marco Nie\Development\VPN\cloudflare\Microsoft Azure - 中国 香港.txt" -o "result-azure.csv"
+".\CloudflareST.exe" -url https://niekun.net/cloud/other/cfspeedtest.png -p 0 -f "C:\Users\Marco Nie\Development\VPN\cloudflare\Oracle Cloud - 日本 东京.txt" -o "result-oracle-jp.csv"
 
 :: start notepad "result-google.csv"
 :: start notepad "result-amazon.csv"
 
 echo.
 echo finded ip:
-type "result-google.csv"
-type "result-amazon.csv"
-type "result-bgp.csv"
-type "result-powerline.csv"
+type "result-google-jp.csv"
+type "result-google-us.csv"
+type "result-amazon-hk.csv"
+type "result-amazon-jp.csv"
+type "result-ctm.csv"
+type "result-hgc.csv"
+type "result-hkt.csv"
+type "result-azure.csv"
+type "result-oracle-jp.csv"
 
 echo.
 echo ip list can be use:
-for /f "delims=," %%a in (' type "result-google.csv" ') do ping -n 1 %%a >nul && (echo %%a ok)
-for /f "delims=," %%a in (' type "result-amazon.csv" ') do ping -n 1 %%a >nul && (echo %%a ok)
-for /f "delims=," %%a in (' type "result-bgp.csv" ') do ping -n 1 %%a >nul && (echo %%a ok)
-for /f "delims=," %%a in (' type "result-powerline.csv" ') do ping -n 1 %%a >nul && (echo %%a ok)
+for /f "delims=," %%a in (' type "result-google-jp.csv" ') do ping -n 1 %%a >nul && (echo %%a ok)
+for /f "delims=," %%a in (' type "result-google-us.csv" ') do ping -n 1 %%a >nul && (echo %%a ok)
+for /f "delims=," %%a in (' type "result-amazon-hk.csv" ') do ping -n 1 %%a >nul && (echo %%a ok)
+for /f "delims=," %%a in (' type "result-amazon-jp.csv" ') do ping -n 1 %%a >nul && (echo %%a ok)
+for /f "delims=," %%a in (' type "result-ctm.csv" ') do ping -n 1 %%a >nul && (echo %%a ok)
+for /f "delims=," %%a in (' type "result-hgc.csv" ') do ping -n 1 %%a >nul && (echo %%a ok)
+for /f "delims=," %%a in (' type "result-hkt.csv" ') do ping -n 1 %%a >nul && (echo %%a ok)
+for /f "delims=," %%a in (' type "result-azure.csv" ') do ping -n 1 %%a >nul && (echo %%a ok)
+for /f "delims=," %%a in (' type "result-oracle-jp.csv" ') do ping -n 1 %%a >nul && (echo %%a ok)
