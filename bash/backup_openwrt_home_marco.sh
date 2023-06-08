5a9f31cc17a63d6eed59a27797334a96683b5d84
Marco Nie <github@niekun.net>
2023-06-08 13:05:36 +0800

    update

--- bash/backup_openwrt_home_marco.sh
+++ bash/backup_openwrt_home_marco.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 
-# script based on instructions from:
-# https://openwrt.org/docs/guide-user/troubleshooting/backup_restore
-
-router_hostname=openwrt.home.marco
-router_passwd=Pmz3qUxWkahy
+# must install sshpass first "apt install sshpass"
 
 # Generate/update backup
 sshpass -p $router_passwd ssh root@$router_hostname 'umask go=; sysupgrade -b /tmp/backup-${HOSTNAME}-$(date +%F).tar.gz'
+if [ "$?" -ne 0 ]; then
+    echo "backup failed."
+    exit 1
+fi
 
 # Download backup
 sshpass -p $router_passwd scp root@$router_hostname:/tmp/backup-*.tar.gz /mnt/h99_home/ftp/OpenwrtBackup
+if [ "$?" -ne 0 ]; then
+    echo "transfer to cloud failed."
+    exit 1
+fi
 
 sshpass -p $router_passwd ssh root@$router_hostname 'umask go=; rm /tmp/backup-*'
+if [ "$?" -ne 0 ]; then
+    echo "remove file failed."
+    exit 1
+fi
--- bash/backup_ubuntu_home_marco.sh
+++ bash/backup_ubuntu_home_marco.sh
@@ -67,6 +67,7 @@ BACKUP[13]="/usr/lib/systemd/system"
 BACKUP[14]="/root/.bashrc"
 BACKUP[15]="/etc/hostname"
 BACKUP[16]="/etc/vsftpd.conf"
+BACKUP[17]="/etc/env_addon"
 
 # Number of days to store daily local backups (default 7 days)
 LOCALAGEDAILIES="1"
