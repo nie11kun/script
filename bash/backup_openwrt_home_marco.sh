d9c5350099121db24042612eb4a1bf0c86c355d4
Marco Nie <github@niekun.net>
2023-06-08 11:51:29 +0800

    update

--- bash/backup_openwrt_home_marco.sh
+++ bash/backup_openwrt_home_marco.sh
@@ -7,9 +7,9 @@ router_hostname=openwrt.home.marco
 router_passwd=132018
 
 # Generate/update backup
-sshpass -p $router_passwd ssh root@$router_hostname 'umask go=; sysupgrade -b /tmp/backup-${HOSTNAME}-$(date +%F).tar.gz'
+sshpass -p $router_passwd ssh root@openwrt.home.marco 'umask go=; sysupgrade -b /tmp/backup-${HOSTNAME}-$(date +%F).tar.gz'
 
 # Download backup
-sshpass -p $router_passwd scp root@$router_hostname -p "$router_passwd":/tmp/backup-*.tar.gz /mnt/h99_home/ftp/OpenwrtBackup
+sshpass -p $router_passwd scp root@openwrt.home.marco -p "$router_passwd":/tmp/backup-*.tar.gz /mnt/h99_home/ftp/OpenwrtBackup
 
-sshpass -p $router_passwd ssh $router_hostname 'umask go=; rm /tmp/backup-*'
+sshpass -p $router_passwd ssh root@openwrt.home.marco 'umask go=; rm /tmp/backup-*'
