45434a8f1a65092a739e47a90fe5aa4ef360fa65
Marco Nie <github@niekun.net>
2023-06-09 14:31:40 +0800

    update

--- bash/backup_openwrt_home_marco.sh
+++ bash/backup_openwrt_home_marco.sh
@@ -2,27 +2,29 @@
 
 # must install sshpass first "apt install sshpass"
 
+LOGFILE="/home/backups/backup_openwrt.log"
+
 # Generate/update backup
 sshpass -p $router_passwd ssh root@$router_hostname 'umask go=; sysupgrade -b /tmp/backup-${HOSTNAME}-$(date +%Y%m%d%H%M%S).tar.gz'
 if [ "$?" -ne 0 ]; then
-    echo "backup failed."
+    echo "backup failed." >> ${LOGFILE}
     exit 1
 fi
-echo "backup done."
+echo "backup done." >> ${LOGFILE}
 
 # Download backup
 sshpass -p $router_passwd sftp root@$router_hostname << EOF
 get /tmp/backup-*.tar.gz /mnt/h99_home/ftp/OpenwrtBackup
 EOF
 if [ "$?" -ne 0 ]; then
-    echo "transfer to cloud failed."
+    echo "transfer to cloud failed." >> ${LOGFILE}
     exit 1
 fi
-echo "transfer done."
+echo "transfer done." >> ${LOGFILE}
 
 sshpass -p $router_passwd ssh root@$router_hostname 'umask go=; rm /tmp/backup-*'
 if [ "$?" -ne 0 ]; then
-    echo "remove file failed."
+    echo "remove file failed." >> ${LOGFILE}
     exit 1
 fi
-echo "remove done."
+echo "remove done." >> ${LOGFILE}
