6afad4cd0f98fd96bb628f7a52dba1971e190f7b
Marco Nie <github@niekun.net>
2023-06-09 14:37:35 +0800

    update

--- bash/backup_openwrt_home_marco.sh
+++ bash/backup_openwrt_home_marco.sh
@@ -4,27 +4,31 @@
 
 LOGFILE="/home/backups/backup_openwrt.log"
 
+log() {
+    echo -e "$(date "+%Y-%m-%d %H:%M:%S")" "$1" >> ${LOGFILE}
+}
+
 # Generate/update backup
 sshpass -p $router_passwd ssh root@$router_hostname 'umask go=; sysupgrade -b /tmp/backup-${HOSTNAME}-$(date +%Y%m%d%H%M%S).tar.gz'
 if [ "$?" -ne 0 ]; then
-    echo "backup failed." >> ${LOGFILE}
+    log "backup failed."
     exit 1
 fi
-echo "backup done." >> ${LOGFILE}
+log "backup done."
 
 # Download backup
 sshpass -p $router_passwd sftp root@$router_hostname << EOF
 get /tmp/backup-*.tar.gz /mnt/h99_home/ftp/OpenwrtBackup
 EOF
 if [ "$?" -ne 0 ]; then
-    echo "transfer to cloud failed." >> ${LOGFILE}
+    log "transfer to cloud failed."
     exit 1
 fi
-echo "transfer done." >> ${LOGFILE}
+log "transfer done."
 
 sshpass -p $router_passwd ssh root@$router_hostname 'umask go=; rm /tmp/backup-*'
 if [ "$?" -ne 0 ]; then
-    echo "remove file failed." >> ${LOGFILE}
+    log "remove file failed."
     exit 1
 fi
-echo "remove done." >> ${LOGFILE}
+log "remove done."
