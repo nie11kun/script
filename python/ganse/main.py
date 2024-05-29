5510c3940b4f7fdfa44ee27205853ae2a25860e6
Marco Nie <github@niekun.net>
2024-05-29 16:34:41 +0800

    update main file

--- python/ganse/main.py
+++ python/ganse/main.py
@@ -1,24 +1,28 @@
 from library.curve_to_wheel_points import curve_to_wheel_points
 from library.calc import remove_leading_whitespace, parse_coordinates, plot_coordinates, read_file
 from decimal import Decimal
+import numpy as np
 
 # 砂轮杆偏移工件中心最大距离
-gan_distance_max = 35.4
+gan_distance_max = 20
 
 # 砂轮杆偏移工件中心最小距离
-gan_distance_min = 35.4
+gan_distance_min = 18
 
 # 砂轮直径步进
-step_dia = 0.2
+step_dia = 1
 
 # 砂轮安装角
-gan_angle = 10.9191
+gan_angle = 3.47
 
 # 工件中径
 mid_dia = 82.5
 
 # 导程
 work_lead = 50
+
+# 滚轮圆弧半径
+dresser_r = 1.0
 
 # dxf
 dxf_file = 'dxf/19.05.dxf'
@@ -37,14 +41,23 @@ for i in range(dia_num+1):
     if i == 0:
         is_plot = True
     else:
-        is_plot = False
+        if i == dia_num:
+            is_plot = True
+        else:
+            is_plot = False
     
-    wheel_dia[i], file_content[i], point_string[i] = curve_to_wheel_points(dxf_file=dxf_file, gan_distance=gan_distance_min+step_dia/2*i, gan_angle=gan_angle, mid_dia=mid_dia, work_lead=work_lead, if_plot=is_plot)
+    wheel_dia[i], file_content[i], point_string[i] = curve_to_wheel_points(dxf_file=dxf_file, gan_distance=gan_distance_min+step_dia/2*i, gan_angle=gan_angle, mid_dia=mid_dia, work_lead=work_lead, dresser_r=dresser_r, if_plot=is_plot)
 
     # 将直径转换为4位小数的字符串 并替换小数点为下划线
     wheel_dia_str[i] = f"{wheel_dia[i]:.4f}".replace('.', '_')
 
 head_content = f"""
+;砂轮直径范围:{wheel_dia[0]:.4f} - {wheel_dia[-1]:.4f}
+;砂轮杆安装角:{gan_angle}
+;工件螺旋升角:{np.rad2deg(np.arctan2(work_lead, np.pi * mid_dia)):.4f}
+;工件中径:{mid_dia}
+;工件导程:{work_lead}
+;滚轮圆弧半径:{dresser_r}
 ;********************************
 DEF REAL VER_MODE,WHEEL_DIA
 DEF AXIS AX_HORI,AX_VER
@@ -66,27 +79,29 @@ IF (WHEEL_DIA<{wheel_dia[dia_num]:.4f}) GOTOF DIA_{wheel_dia_str[dia_num]};
 ;********************************
 """
 
+file_name = f"GS_{mid_dia}_{work_lead}".replace('.', '_') + '.SPF'
+
 # 将内容写入文件
-with open("output.txt", "w", encoding="utf-8") as f:
+with open(file_name, "w", encoding="utf-8") as f:
     f.write(head_content)
 
 for i in range(dia_num+1):
     # 将内容写入文件
-    with open("output.txt", "a", encoding="utf-8") as f:
+    with open(file_name, "a", encoding="utf-8") as f:
         f.write(file_content[i])
 
 # 删除文本中的缩进
-remove_leading_whitespace("output.txt","output.txt")
+remove_leading_whitespace(file_name,file_name)
 
-# 文件路径
-file1_path = 'file1.txt'
+# 比较文件路径
+# file1_path = 'file1.txt'
 
 # 读取文件内容
-str1 = read_file(file1_path)
+# str1 = read_file(file1_path)
 
 # 解析坐标
-points1 = parse_coordinates(str1)
-points2 = parse_coordinates(point_string[0])
+points1 = parse_coordinates(point_string[0])
+points2 = parse_coordinates(point_string[1])
 
 # 绘制坐标
 plot_coordinates(points1, points2)
